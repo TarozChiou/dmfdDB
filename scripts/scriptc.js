711a1ae1517ad6271b89c001607b6f0d7723b834
TarozChiou <62319549+TarozChiou@users.noreply.github.com>
2020-05-18 13:00:41 +0800

    -

---
+++ scripts/scriptc.js
@@ -0,0 +1,8 @@
+// function empBtn() {
+//     document.querySelector(".empBtn").classList.toggle("active")
+// }
+
+function empBtn() {
+    var empBox = document.querySelector(".empBox");
+    empBox.classList.toggle("active");
+}
